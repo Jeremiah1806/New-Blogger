ea4a6f9c802ffcfd2c03656e7a394d8c48f1750c
Jeremiah1806 <jeremiahfg18@gmail.com>
2026-06-30 11:24:17 +0530

    fix login function cleanup

--- NewBlogger/settings.py
+++ NewBlogger/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ki#30klgp0cu8!pe08e3b17!#uw(8axrrusi3ml!vl!mt00nd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
